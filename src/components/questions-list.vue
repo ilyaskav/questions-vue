9c38d3819456e6ea26a303eb6de5f076fd4d206f
Ilya Skavronskiy <ilya.skav@gmail.com>
2020-06-29 15:44:21 +0300

    created project

---
+++ src/components/questions-list.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Popular</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Recent</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Unanswered</a>
+      </li>
+    </ul>
+    <div v-for="question in questions" :key="question">{{ question.question }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    questions: Array
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home container-md">
+    <div class="row">
+      <div class="col-md">
+        <div class="form-row question-input">
+          <div class="col-md-10">
+            <input type="text" placeholder="What do you want to know" class="form-control" />
+          </div>
+          <div class="col-md-2">
+            <button type="button" class="btn btn-primary">Ask question</button>
+          </div>
+        </div>
+        <questions-list :questions="questions"></questions-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import QuestionsList from '@/components/questions-list.vue';
+
+export default {
+  name: 'home',
+  components: {
+    QuestionsList
+  },
+  data() {
+    return {
+      questions: [{ question: 'Как мне увидеть оповещение о вопросе?' }]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.question-input {
+  background-color: #f2f2f2;
+  padding: 20px 0;
+}
+</style>
